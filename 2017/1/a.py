f0602b0b648a422f2bf865d58be55d009a44a6d8
rincewind <leonard.volaric-horvat@fer.hr>
2017-12-01 11:14:35 +0100

    1.b done

--- 2017/1/a.py
+++ 2017/1/a.py
@@ -1,10 +1,18 @@
+class CircularList(list):
+	def __getitem__(self, key):
+		return list.__getitem__(self, key % len(self))
+
+	def __setitem__(self, key, item):
+		list.__setitem__(self, key % len(self), item)
+
+
 total = 0
-in_string = input()
+in_string = CircularList(input())
+step = int(len(in_string) / 2)
 
 for i in range(len(in_string)):
-	print("Last: ", in_string[i])
 	curr_digit = in_string[i]
-	next_digit = in_string[0] if i == len(in_string) - 1 else in_string[i + 1]
+	next_digit = in_string[i + step]
 	if curr_digit == next_digit:
 		total += int(curr_digit)
 
