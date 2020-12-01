0eccd31d23468a29cf27116c7d40598bf9924c93
LVH-27 <reo.the.yokel@gmail.com>
2020-12-01 11:40:07 +0100

    Added 2020 day 1 solutions and input

---
+++ 2020/1/a.py
@@ -0,0 +1,11 @@
+from itertools import combinations
+from math import prod
+
+with open("input", "r") as f:
+    numbers = [int(line) for line in f.readlines()]
+
+tuplet_number = 2
+for tuplet in combinations(numbers, tuplet_number):
+    if sum(tuplet) == 2020:
+        print(prod(tuplet))
+
