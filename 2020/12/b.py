ad265ef4f227d52ae43725c0d6aae12306261dc6
LVH-27 <reo.the.yokel@gmail.com>
2020-12-13 15:31:53 +0100

    Added part 2 solution

--- 2020/12/b.py
+++ 2020/12/b.py
@@ -1,9 +1,37 @@
+from math import sqrt, atan, sin, cos, radians, degrees, pi
+
+
 with open("input", 'r') as f:
     lines = f.readlines()
 
 
 def manhattan(start, end):
     return abs(end[0] - start[0]) + abs(end[1] - start[1])
+
+
+def rotate_waypoint(way_pos, angle):
+    def calc_phi(point):
+        if point[0] == 0:
+            phi = 90 if point[1] > 0 else 270
+        else:
+            phi = degrees(atan(point[1] / point[0]))
+            if point[0] < 0:
+                phi = 180 + phi
+        return phi
+
+    print(way_pos)
+    calc_r = lambda t: sqrt(t[0] ** 2 + t[1] ** 2) 
+    # calc_phi = lambda t: degrees(atan(t[1] / t[0])) if t[0] != 0 else (90 if t[1] > 0 else 270)
+    calc_point = lambda r, phi: (round(cos(phi) * r), round(sin(phi) * r))
+
+    r = calc_r(way_pos)
+    phi = calc_phi(way_pos)
+
+    new_phi = phi + angle
+    print(phi, new_phi, angle)
+    new_waypoint = calc_point(r, radians(new_phi))
+    print(new_waypoint)
+    return new_waypoint 
 
 
 directions = {
@@ -18,9 +46,8 @@ actions = {
         'S': lambda distance, cur_pos: (cur_pos[0], cur_pos[1] - distance),
         'E': lambda distance, cur_pos: (cur_pos[0] + distance, cur_pos[1]),
         'W': lambda distance, cur_pos: (cur_pos[0] - distance, cur_pos[1]),
-        'L': lambda degrees, direction: (direction + degrees) % 360,
-        'R': lambda degrees, direction: (direction - degrees) % 360,
-        'F': lambda distance, direction, cur_pos, waypoint_pos: actions[directions[direction]](distance, cur_pos)
+        'L': lambda angle, waypoint_pos: rotate_waypoint(waypoint_pos, angle),
+        'R': lambda angle, waypoint_pos: rotate_waypoint(waypoint_pos, -angle),
         'F': lambda multiplier, cur_pos, waypoint_pos: (cur_pos[0] + waypoint_pos[0] * multiplier, cur_pos[1] + waypoint_pos[1] * multiplier)
     }
 
@@ -33,18 +60,17 @@ waypoint_start = (10, 1)
 waypoint_cur = waypoint_start
 
 for line in lines:
-    print(line)
+    print(line.strip())
     action = line[0]
     value = int(line[1:])
     
     if action in "NEWS":
         waypoint_cur = actions[action](value, waypoint_cur)
     elif action in "LR":
-        direction = actions[action](value, direction)
+        waypoint_cur = actions[action](value, waypoint_cur)
     else:
         cur_pos = actions[action](value, cur_pos, waypoint_cur)
-    print(cur_pos, direction)
-    #input()
+    print(cur_pos, waypoint_cur, direction)
 
 print(cur_pos, direction, line)
 print(manhattan(start_pos, cur_pos))
